a0ca8311cc3079678540a2e6e72ab7829504337e
Rahul Pandey <rahulpandey862@gmail.com>
2022-12-08 11:08:03 +0530

    Initial Checkin - App Services Learning

---
+++ Models/UserSettings.cs
@@ -0,0 +1,8 @@
+namespace app_service.Models;
+
+public class UserSettings
+{
+    public string? Mode { get; set; }
+    
+    public string? Platform { get; set; }
+}
---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using app_service.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+
+namespace app_service.Pages;
+
+public class PrivacyModel : PageModel
+{   
+    public  string? Mode { get; set; }
+    public  string? Development { get; set; }
+
+    private readonly ILogger<PrivacyModel> _logger;
+    private readonly IOptions<UserSettings> _userSettings;
+
+    public PrivacyModel(
+        ILogger<PrivacyModel> logger,
+        IOptions<UserSettings> userSettings)
+    {
+        _logger = logger;
+        _userSettings = userSettings;
+    }
+
+    public void OnGet()
+    {
+        Mode = _userSettings.Value.Mode;
+        Development = _userSettings.Value.Platform;
+    }
+}
+
